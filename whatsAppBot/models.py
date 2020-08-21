5938c46f7a460da94e6478269f667e2fe34e8abb
cettipao <cettipao@live.com.ar>
2020-08-20 22:54:21 -0300

    Saving numbers system

--- whatsAppBot/models.py
+++ whatsAppBot/models.py
@@ -1,3 +1,8 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Talker(models.Model):
+    number = models.CharField(max_length=30)
+    dateCreated = models.DateField(auto_now=True)
--- whatsAppBot/views.py
+++ whatsAppBot/views.py
@@ -1,11 +1,18 @@
 from django.shortcuts import render
 from django_twilio.decorators import twilio_view
 from twilio.twiml.messaging_response import MessagingResponse
+from .models import Talker
 
 @twilio_view
 def smsView(request):
+    number = request.POST.get('From','')
+    if number in Talker.objects.all():
+        msg = "Bienvenido de Nuevo, intentado conectar..."
+    else:
+        msg = "Bienvenido, intentado conectar..."
+        Talker.objects.create(number)
     name = request.POST.get('Body', '')
-    msg = 'Hey %s, how are you today?' % (name)
+    #msg = 'Hey %s, how are you today?' % (name)
     r = MessagingResponse()
     r.message(msg)
     return r
