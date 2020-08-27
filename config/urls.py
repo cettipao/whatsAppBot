408cebd3d4e869ed2b26dfbc206b65523e4ca7c4
cettipao <cettipao@live.com.ar>
2020-08-27 19:51:49 -0300

    Solving static problems 3

--- config/urls.py
+++ config/urls.py
@@ -24,4 +24,4 @@ urlpatterns = [
     path('admin/', admin.site.urls),
 ]
 
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
